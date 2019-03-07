47b893f68c675068f6db0167a3b6ab6261b9cd16
Terra Vermillion <VermillionT@outlook.com>
2019-03-07 11:26:44 -0700

    Created BooksDbContext, Added Books as DbSet

---
+++ BookShelf/BookShelf/Data/BooksDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BookShelf.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Data
+{
+    /// <summary>
+    /// Represents the Database Interface for manipulating book resources
+    /// </summary>
+    public class BooksDbContext : DbContext
+    {
+        /// <summary>
+        /// An entity set that can be used to manipulate books in the database
+        /// </summary>
+        public DbSet<Book> Books { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="BooksDbContext"/>
+        /// </summary>
+        /// <param name="options">The Options for the DbContext using this <see cref="BooksDbContext"/> as the Context</param>
+        public BooksDbContext(DbContextOptions<BooksDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
