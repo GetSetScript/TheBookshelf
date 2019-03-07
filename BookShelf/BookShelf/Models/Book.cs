acefdc30b5d4360e869303716b738aeebc6d4f63
Terra Vermillion <VermillionT@outlook.com>
2019-03-07 11:09:44 -0700

    Added Book POCO, Added Data Annotations

---
+++ BookShelf/BookShelf/Models/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    /// <summary>
+    /// Represent a book
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// The title of the book
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The description of the book
+        /// </summary>
+        [Required]
+        [MinLength(10)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The date the book was read
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateTime DateRead { get; set; }
+
+        /// <summary>
+        /// The 5 star rating for the book
+        /// </summary>
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        /// <summary>
+        /// The image URL for the book
+        /// </summary>
+        [DataType(DataType.Url)]
+        public string ImageUrl { get; set; }
+    }
+}
