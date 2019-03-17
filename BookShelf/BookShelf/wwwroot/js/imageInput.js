8fab63770bed51394b401520addc3df8e521c249
Terra Vermillion <VermillionT@outlook.com>
2019-03-17 17:21:10 -0600

    Added CSS and Html, Added client side validation to form, created form as partial view, test, fixed bug with datetime not being nullable, updated database

---
+++ BookShelf/BookShelf/wwwroot/js/imageInput.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    let imageContentElement = document.querySelector(".p-addEditBookForm_imageContent");
+    let imageInputElement = document.querySelector(".p-addEditBookForm_imageInput");
+
+    imageInputElement.addEventListener("change", function (e) {
+
+        fileName = e.target.value.split('\\').pop();
+
+        imageContentElement.innerHTML = fileName;
+    });
+
+    $.validator.addMethod("fileSize", function (value, element) {
+        let twoMB = 2097152;
+        return this.optional(element) || element.files[0].size <= twoMB;
+    }, "file size must be less than 2mb");
+
+    $.validator.addMethod("fileExtension", function (value, element) {
+        let extensions = "png|jpeg|jpg";
+        return this.optional(element) || value.match(new RegExp("\\.(" + extensions + ")$", "i"));
+    }, "The image must have a file extension of png, jpeg, or jpg");
+    
+})();
+
